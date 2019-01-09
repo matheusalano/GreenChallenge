4e1cda7dd8e132bf529e643fa8f112fc0b840b25
Matheus Alano <matheus.alano.dias@gmail.com>
2019-01-09 18:31:45 -0200

    Project created.

---
+++ GreenChallengeTests/GreenChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  GreenChallengeTests.swift
+//  GreenChallengeTests
+//
+//  Created by Matheus Alano on 09/01/19.
+//  Copyright © 2019 Matheus Alano. All rights reserved.
+//
+
+import XCTest
+@testable import GreenChallenge
+
+class GreenChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
