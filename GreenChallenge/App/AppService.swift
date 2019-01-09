d5d230aa986c4bcbd7be4b08011cc79705aa68ed
Matheus Alano <matheus.alano.dias@gmail.com>
2019-01-09 19:02:33 -0200

    Networking Layer

---
+++ GreenChallenge/App/AppService.swift
@@ -0,0 +1,58 @@
+//
+//  AppService.swift
+//  GreenChallenge
+//
+//  Created by Matheus Alano on 09/01/19.
+//  Copyright © 2019 Matheus Alano. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+enum HTTPMethod: String {
+    case GET, POST
+}
+
+enum ServiceError: Error {
+    case cannotParse
+    case cannotParseParameters
+    case invalidURL
+}
+
+class AppService {
+    
+    private let baseUrl = "http://5b840ba5db24a100142dcd8c.mockapi.io/"
+    private let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func request<T: Codable>(path: String, method: HTTPMethod, parameters: [String: Any]? = nil) -> Observable<T> {
+        guard let url = URL(string: baseUrl + path) else { return Observable.error(ServiceError.invalidURL) }
+        var urlRequest = URLRequest(url: url)
+        
+        if let parameters = parameters {
+            do {
+                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            } catch _ {
+                return Observable.error(ServiceError.cannotParseParameters)
+            }
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        urlRequest.httpMethod = method.rawValue
+        
+        return session.rx
+            .json(request: urlRequest)
+            .flatMap({ json throws -> Observable<T> in
+                do {
+                    let jsonData = try JSONDecoder().decode(T.self, from: JSONSerialization.data(withJSONObject: json, options: .prettyPrinted))
+                    
+                    return Observable.just(jsonData)
+                } catch _ {
+                    return Observable.error(ServiceError.cannotParse)
+                }
+            })
+    }
+}
