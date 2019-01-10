24020bfc631c72bb60ab6a6d89c605f6d9bfc457
Matheus Alano <matheus.alano.dias@gmail.com>
2019-01-10 18:58:32 -0200

    Event class' name changed to GCEvent because of ambiguity. All view model's inputs tested.

---
+++ GreenChallenge/General/Extensions/UIViewController+Alerts.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Alerts.swift
+//  GreenChallenge
+//
+//  Created by Matheus Alano on 10/01/19.
+//  Copyright © 2019 Matheus Alano. All rights reserved.
+//
+
+import Foundation
